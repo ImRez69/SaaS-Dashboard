cfc205af2dbbe8483ef7ed6bdcbfa1ecb3c8b16f
ImRez69 <CaptianAlirez@gmail.com>
2026-07-23 20:41:03 +0330

    Added Left Todos to Topbar

--- src/components/layouts/Topbar.jsx
+++ src/components/layouts/Topbar.jsx
@@ -1,14 +1,24 @@
 import { sidebarListItems } from "../../data/SidebarData";
+import { useTodos } from "../../components/practices/todos/utils/TodosContext";
 import Avatar from "../ui/avatar";
 import Search from "../ui/Search";
 import ThemeToggle from "../ui/ThemeToggle";
+
 export default function Topbar({ activeId }) {
   const activeTab = sidebarListItems.find((list) => list.id === activeId);
+  const activeTodos = useTodos().filter((todo) => todo.status === false);
 
   return (
     <header className="bg-surface border-border sticky top-0 z-10 flex h-16 items-center justify-between border-b px-6">
-      <Search />
+      <div className="flex items-center gap-5">
+        <Search />
+        <span className="opacity-50">
+          تعداد کار ها: <strong> {activeTodos.length}</strong>
+        </span>
+      </div>
+
       <h3 className="word-spacing-hover-anime">{activeTab.title}</h3>
+
       <div className="flex gap-4">
         <Avatar src="https://i.pravatar.cc/150?img=50" alt={"ممد قلی"} />
         <ThemeToggle border={false} />
